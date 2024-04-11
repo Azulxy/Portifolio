aaff6f7090992411f8bf69969d27e062ba3b7f8d
João Victor B. Azulay <jvazulay@outlook.com>
2024-04-11 16:36:20 -0300

    atualização contatos

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function hideSidebar() {
     const sidebar = document.querySelector('.sidebar');
     sidebar.classList.remove('active'); 
 }
+function redirectToTalkToMe() {
+    window.location.href = "https://w.app/joaoazulxy";
+}
+// let ZapButton = document.getElementById("ZapButton");
+
+// ZapButton.onclick = function() {
+//     document.body.classList.toggle("ZapButton");
+//     if (document.body.classList.contains("ZapButton")) {
+//         ZapButton.border = "0";
+//     } else {
+//         ZapButton.border = "";
+//         }
+//     }
